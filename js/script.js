5ae5e34b3f9984ec8168e6d7544348bf8da0b3ae
Malacai Hiebert <malacaihiebert@gmail.com>
2022-01-12 18:52:41 -0600

    tyring somehitn

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 $(".navbar").load("../common/nav.html");
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
