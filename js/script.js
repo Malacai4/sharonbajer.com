7a0f7270d273462920997741d7d653743311af9e
Malacai Hiebert <malacaihiebert@gmail.com>
2022-01-23 11:08:33 -0600

    working on animations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 $(".navbar").load("../common/nav.html");
+
+const imgs = document.querySelectorAll("section img");
+let divIndex = 0;
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    var $sec = $(".container section")[divIndex];
+    console.log($sec);
+
+    if (entry.isIntersecting) {
+      $sec.classList.remove("unslide-anim");
+      $sec.classList.add("slide-anim");
+      return; // if we added the class, exit the function
+    }
+    $sec.classList.remove("slide-anim");
+    $sec.classList.add("unslide-anim");
+  });
+});
+
+imgs.forEach((i) => {
+  observer.observe(i);
+  divIndex = imgs.indexOf(i);
+});
